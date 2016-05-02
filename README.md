77d514726f199399f21830d6433cbb77539b9f96
Corey Holmes <coreyholmes20@gmail.com>
2016-05-02 10:09:04 -0700

    Navbar

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-A ready to go template for whatever you want. Use it as you wish :)
-
+Pebble Piano
 Made by: @corey604
-
-Includes:	
-• Bootstrap
-• jQuery
-• Font Awesome
-• ION Icons
-• IE10 viewport hack
--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 /* Enter Javascript below */
+$(function(){
+
+  // jQuery.fn.shake = function() {
+  //   this.each(function(i) {
+  //     $(this).css({ "position" : "relative" });
+  //     for (var x = 1; x <= 1; x++) {
+  //       $(this).animate({ left: -10 }, 100).animate({ left: 0 }, 50).animate({ left: 10 }, 100).animate({ left: 0 }, 50);
+  //     }
+  //   });
+  //   return this;
+  // }
+
+  // setInterval(function(){
+  //   $('.shake').shake();
+  // },5000);
+
+});
